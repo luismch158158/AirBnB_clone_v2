1597b5abb2001e8771601ab8806dc53112187be1
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-10 14:12:43 -0500

    Feat: Adding attributtes for SQLAlchemy and fixing some errors

--- models/base_model.py
+++ models/base_model.py
@@ -29,13 +29,25 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if (not kwargs.get("id", 0)):
+                self.id = str(uuid.uuid4())
+            if (kwargs.get("created_at", 0)):
+                kwargs["created_at"] = datetime.fromisoformat(
+                    kwargs["created_at"])
+            else:
+                self.created_at = datetime.now()
+            if (kwargs.get("updated_at", 0)):
+                kwargs["updated_at"] = datetime.fromisoformat(
+                    kwargs["updated_at"])
+            else:
+                self.updated_at = self.created_at
+            # kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            #                                          '%Y-%m-%dT%H:%M:%S.%f')
+            # kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            #                                          '%Y-%m-%dT%H:%M:%S.%f')
+            if (kwargs.get("__class__", 0)):
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
