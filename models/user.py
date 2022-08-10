cc02ead8ef74e3c0f70527327bf9a420b699a3f7
Christian Varas <christianvaras.2020@gmail.com>
2022-08-10 14:32:54 -0500

    Adding attributes SQLAlchemy for user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if models.storage_type == 'db':
+        __tablename__ = "users"
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user", cascade='all')
+        reviews = relationship("Review", backref="user", cascade='all')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
