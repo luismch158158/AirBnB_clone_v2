b62ed9f11f976b4052ceb7352319e414e088f052
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-10 14:10:47 -0500

    Feat: Adding attributtes for SQLAlchemy

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from models.review import Review
+import models
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+
+    if models.storage_type == 'db':
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
