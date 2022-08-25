437a21118e1c7616ff96ed022b42ecb1b11b1c51
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-25 11:58:12 -0500

    changin order of chown

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,10 +5,9 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-chown -hR ubuntu:ubuntu /data/
 echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
+chown -hR ubuntu:ubuntu /data/
 search="^\t\}$"
 adding="\t\}\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
 
