eacb4c641a9308e290675b8400c55e7804173756
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-09-11 12:55:21 -0500

    adding other state.py

--- models/state.py
+++ models/state.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 from models.city import City
 import models
 
 
 class State(BaseModel, Base):
     """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=True)
 
-    if models.storage_type == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete')
+
     else:
-        name = ""
-
         @property
         def cities(self):
+            """return new_list"""
             new_list = []
-            cities_all = models.storage.all(City)
-            for value in cities_all.values():
-                if (value.state_id == self.id):
-                    new_list.append(value)
+
+            value = models.storage.all(City).values()
+            for i in value:
+                if self.id == i.state_id:
+                    new_list.append(i)
+
             return new_list
