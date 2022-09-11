9466716d5157875521c56ddb1e0689a2a11e3577
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-09-11 10:28:35 -0500

    Adding number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Script to starts a Flask web application"""
+from flask import Flask, render_template
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Return message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_HBNB():
+    """Return message HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def variable_text(text):
+    """Return text variable"""
+    new_text = text.replace('_', ' ')
+    return ('C {:s}'.format(escape(new_text)))
+
+
+@app.route("/python/<string:text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def variable_text_default(text="is cool"):
+    """Return text variable with default"""
+    new_text = text.replace('_', ' ')
+    return ('Python {:s}'.format(escape(new_text)))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_int(n):
+    """Return number"""
+    return ('{} is a number'.format(escape(n)))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Display a HTML page only if n is a integer"""
+    return render_template('5-number.html', num_1=escape(n))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
