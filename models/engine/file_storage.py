8f11fc26b1fce21bf612a2dbf014fe6411dd0bec
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-09-11 10:50:03 -0500

    Adding improve engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,7 @@ class FileStorage:
             key_dict = obj.__class__.__name__ + '.' + obj.id
             if key_dict in self.__objects:
                 del self.__objects[key_dict]
+
+    def close(self):
+        """Method call to reload"""
+        self.reload()
