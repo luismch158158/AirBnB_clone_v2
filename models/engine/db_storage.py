ba3f2b533adfdf9228f20007c46187d7cdac0e5a
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-09-11 20:38:50 -0500

    removing parenthesis

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,7 +69,7 @@ class DBStorage:
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """call remove() method on the private session attribute"""
